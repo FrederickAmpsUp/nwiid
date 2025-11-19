2f28e138d29f6841846de925a8989310dcb1dfe1
Frederick Ziola <frederick.ziola@gmail.com>
2025-11-19 13:27:35 -0500

    Update copyright message

--- impl/connect.c
+++ impl/connect.c
@@ -1,4 +1,5 @@
 /* Copyright (C) 2007 L. Donnie Smith <donnie.smith@gatech.edu>
+ *               2025 Frederick Ziola <frederick.ziola@gmail.com>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
